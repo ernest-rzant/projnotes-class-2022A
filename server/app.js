c58107c34a75862911af69d4f8f0420b46d9857b
ernest-rzant <ernestorodriguez643@gmail.com>
2022-03-02 11:26:38 -0600

    App updated to modern js

--- server/app.js
+++ server/app.js
@@ -1,23 +1,24 @@
 // Preambulo
 // Ayuda a manejar errores http
-var createError = require('http-errors');
+import createError from 'http-errors';
 // Ayuda a crear servidores web
-var express = require('express');
+import express from 'express';
 // Nucleo de node, ayuda al manejo de las rutas
-var path = require('path');
+import path from 'path';
 // Ayuda al manejo e cookies
-var cookieParser = require('cookie-parser');
+import cookieParser from 'cookie-parser';
 // Maneja el log de peticiones http
-var logger = require('morgan');
+import logger from 'morgan';
 
 // Las rutas
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var usersRouter = require('./routes/about');
+//var indexRouter = require('./routes/index');
+import indexRouter from './routes/index'
+import usersRouter from './routes/users';
+import aboutRouter from './routes/about';
 
 // Aqui se crea la instancia de express
 // (req, res, next)
-var app = express();
+const app = express();
 // Configuracion del motor de pantillas (templae Engine)
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -35,15 +36,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Registrando las rtas en la APP
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/about', usersRouter);
+app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -53,4 +54,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+//module.exports = app;
+//Exportando instancia de app
+// usando js moderno
+export default app;
