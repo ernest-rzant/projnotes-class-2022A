bfd5434f04b3fb24f4447731a1e1496075840be8
ernest-rzant <ernestorodriguez643@gmail.com>
2022-03-30 07:56:01 -0600

    Testings de ES06 en Front-end

--- client/index.js
+++ client/index.js
@@ -1,1 +1,24 @@
 console.log("Webpack Working!!! 🎁");
+
+// default parameters
+let show = (m = "Hola") => {
+    console.log(m);
+};
+show();
+
+// Promises
+function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('funtion resolve')
+        }, 10000)
+    });
+}
+
+async function asyncCall() {
+    console.log("Calling asyn funtion!!!");
+    const result = await resolveAfter2Seconds();
+    console.log(result); // Imprime "funtion resolve" en la consola
+}
+
+asyncCall();
