ffb115f863b52195080edd677762b9a715514c33
ernest-rzant <ernestorodriguez643@gmail.com>
2022-04-10 19:49:50 -0500

    Inicializando Guia Managing Webpack Dependencies

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -5,9 +5,6 @@ const path = require('path');
 // Plugins para Webpack
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-// Agregando soporte para ESLINT
-const EslintWebpackPluging = require('eslint-webpack-plugin');
-
 module.exports = {
     // 1. Especificar el archivo de entrada
     entry: './client/index.js', 
@@ -70,6 +67,5 @@ module.exports = {
         new MiniCssExtractPlugin({
         filename: path.join("stylesheets", "styles.css"),
     }),
-    new EslintWebpackPluging()
   ]
 }
