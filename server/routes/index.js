51c47e8d299891d5bef94c25f3447396758cafbd
ernest-rzant <ernestorodriguez643@gmail.com>
2022-05-09 08:49:53 -0500

    S32 Favicon Corrigiendo babel minifier

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
   // render manda a renderizar (generar y entregar)
   // la vista al cliente
   // Este es el View-Model
-  res.render('index', { title: 'Express', author: 'Ernest Rodriguez' });
+  res.render('index', { title: 'Projnotes', author: 'Ernest Rodriguez' });
 });
 
 module.exports = router;
