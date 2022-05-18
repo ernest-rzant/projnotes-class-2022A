f1a7374db2a914388357e05280610c0e5340ba78
ernest-rzant <ernestorodriguez643@gmail.com>
2022-05-18 08:26:31 -0500

    S38 Barra de navegacion movil

--- client/index.js
+++ client/index.js
@@ -1,31 +1,14 @@
 /* eslint-disable no-console */
+/* global M */
 
 // Incorporando estilos
 // a mi bundle
 // import './styles/style.css';
 import './styles/mystyle.css';
 
-console.log('🎁 Frond-End Working!!!');
-
-// default parameters
-const show = (m = '😝') => {
-  console.log(m);
-};
-show();
-
-// Promises
-function resolveAfter2Seconds() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('funtion resolve');
-    }, 10000);
-  });
-}
-
-async function asyncCall() {
-  console.log('Calling async funtion!!!');
-  const result = await resolveAfter2Seconds();
-  console.log(result); // Imprime "funtion resolve" en la consola
-}
-
-asyncCall();
+/* Inicializando elementos de materializecss */
+document.addEventListener('DOMContentLoaded', () => {
+  // Obteniendo la rreferencia a la barra de navegacion lateral
+  const sideNav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sideNav);
+});
