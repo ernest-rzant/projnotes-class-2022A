66e5b12a1af506f584467e50fca255e916eed12d
ernest-rzant <ernestorodriguez643@gmail.com>
2022-05-31 23:27:42 -0500

    S42 Agregar rutas de Projects

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,11 +1,16 @@
 // Importando el enrutador de Home
 import homeRouter from './homeRouter';
 
+// Importando el enrutador de Project
+import projectRouter from './projectRouter';
+
 // Funcion que agrega todos los enrutadores
 // a la aplicacion de express
 const addRoutes = (app) => {
   /* Agregando el enrutador  a Home */
   app.use('/', homeRouter);
+  /* Agregando al enrutador Project */
+  app.use('/projects', projectRouter);
 };
 
 /* GET home page. */
